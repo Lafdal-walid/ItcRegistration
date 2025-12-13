85a0d2abad628e657a842b327dd913eea850cc78
Lafdal-walid <lafdaloualid50@gmail.com>
2025-12-13 11:37:36 +0100

    add agenda and sponsors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,13 @@ import {
   List,
   ListItem,
   ListItemText,
+  Typography,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import EventDescription from "../components/EventDescription.jsx";
+import Logo3 from "../assets/Logo3.svg";
+import CodeandSens from "../assets/code & sens.svg";
+import Agenda from "../assets/Agenda.svg";
 
 const Home = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
@@ -18,7 +22,7 @@ const Home = () => {
 
   React.useEffect(() => {
     setIsMobile(window.innerWidth < 992);
-    
+
     const handleResize = () => {
       setIsMobile(window.innerWidth < 992);
     };
@@ -116,6 +120,184 @@ const Home = () => {
       </Drawer>
       <ToRegister />
       <EventDescription pb="900px" />
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "40px",
+          marginBottom: "20px",
+        }}
+      >
+        <img
+          src={Logo3}
+          alt="Logo3"
+          style={{
+            maxWidth: "40px",
+            height: "40px",
+          }}
+        />
+      </Box>
+
+      <Typography
+        variant="body1"
+        sx={{
+          fontFamily: "Poppins",
+          fontSize: "30px",
+          fontWeight: 500,
+          marginBottom: "20px",
+          opacity: 0.9,
+          color: "#BAB9B9",
+          marginTop: "20px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Hosted in Partnership with
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "20px",
+          marginBottom: "20px",
+        }}
+      >
+        <img
+          src={CodeandSens}
+          alt="CodeandSens"
+          style={{
+            maxWidth: "100%",
+            height: "60px",
+          }}
+        />
+      </Box>
+
+      <Typography
+        variant="body1"
+        sx={{
+          fontFamily: "Poppins",
+          fontSize: "16px",
+          fontWeight: 400,
+          marginBottom: "20px",
+          opacity: 0.9,
+          color: "#BAB9B9",
+          marginTop: "30px",
+          textAlign: "center",
+        }}
+      >
+        We are proud to collaborate with Code & Sens, a leading private school
+        offering accessible, flexible, and career-<br></br>oriented training.
+        Their mission is to make tech education available to everyone . Thanks
+        to their adaptable, hands-<br></br>on learning approach, Code & Sens is
+        the perfect environment to host a competition that empowers beginners to{" "}
+        <br></br>grow, create, and challenge themselves.
+      </Typography>
+
+      <Typography
+        variant="body1"
+        sx={{
+          fontFamily: "Poppins",
+          fontSize: "40px",
+          fontWeight: 500,
+          marginBottom: "20px",
+          opacity: 0.9,
+          color: "#BAB9B9",
+          marginTop: "70px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Our Sponsors
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexWrap: "wrap",
+          gap: "100px",
+          marginTop: "40px",
+          marginBottom: "50px",
+        }}
+      >
+        {[1, 2, 3, 4, 5].map((item) => (
+          <Box
+            key={item}
+            sx={{
+              width: "100px",
+              height: "100px",
+              borderRadius: "50%",
+              backgroundColor: "#D9D9D966",
+              flex: "0 0 auto",
+            }}
+          />
+        ))}
+      </Box>
+
+      <Typography
+        variant="body1"
+        sx={{
+          fontFamily: "Poppins",
+          fontSize: "45px",
+          fontWeight: 600,
+          marginBottom: "20px",
+          opacity: 0.9,
+          color: "#BAB9B9",
+          marginTop: "100px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Agenda
+      </Typography>
+
+      <Box
+        sx={{
+          fontFamily: "Poppins",
+          fontSize: "16px",
+          fontWeight: 400,
+          mb: "20px",
+          mt: "20px",
+          opacity: 0.9,
+          color: "#BAB9B9",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "4px",
+        }}
+      >
+        <span>The Great challenge of ITCP 4.0 will be at</span>
+        <span style={{ color: "#E61707", fontWeight: 500 }}>
+          21-22 December
+        </span>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "50px",
+          marginBottom: "50px",
+        }}
+      >
+        <img
+          src={Agenda}
+          alt="Agenda"
+          style={{
+            maxWidth: "auto",
+            height: "auto",
+          }}
+        />
+      </Box>
     </>
   );
 };
